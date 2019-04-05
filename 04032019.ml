f45342f28a8c3f8a1b6b20ff875c645d1577e796
pashatsyganenko <pashatsyganenko@yandex.ru>
2019-04-05 12:27:00 +0300

    Update 04032019.ml

--- 04032019.ml
+++ 04032019.ml
@@ -3,3 +3,9 @@ let rec buildy n s = if n <=0 then s else buildy (n-1) (s^(String.make n ' ')^"*
 let build n = (buildx n "" 0)^(buildy (n-2) "") ;;
 
 print_string (build 40)
+
+let rec repeat str n s = if n<=0 then s else repeat str (n-1) (str^s) ;;
+
+let draw_quad n = if n=0 then "" else if n=1 then "*" else (String.make n '*')^"\n"^(repeat ("*"^(repeat " " (n-2) "")^"*\n") (n-2) "")^(String.make n '*')^"\n" ;;
+
+print_string (draw_quad 8);;
