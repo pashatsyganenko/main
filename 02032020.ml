635f6f59974a8b706bbc004b93ee5aa4380112ef
pashatsyganenko <pashatsyganenko@yandex.ru>
2020-03-11 11:13:21 +0300

    Update 02032020.ml

--- 02032020.ml
+++ 02032020.ml
@@ -1,4 +1,4 @@
-type lambda = Var of string | Ayy of lambda*lambda | Abs of string*lambda ;;
+type lambda = Variable of string | Application of lambda*lambda | Abstraction of string*lambda ;;
 
 let rec list_to_string l = match l with
     | [] -> ""
@@ -11,9 +11,9 @@ let rec string_from_to str x y = let a = ref "" in
     !a ;;
 
 let rec rep a b l = match l with
-    | Var x -> if x = a then Var b else Var x
-    | Ayy (x,y) -> Ayy(rep a b x,rep a b y)
-    | Abs (x,k) -> Abs((if x = a then b else x), rep a b k) ;;
+    | Variable x -> if x = a then Variable b else Variable x
+    | Application (x,y) -> Application(rep a b x,rep a b y)
+    | Abstraction (x,k) -> Abstraction((if x = a then b else x), rep a b k) ;;
 
 let print_bool b =
     if b
@@ -24,45 +24,45 @@ let rec razbor str = if str.[0] = '(' then
         if str.[1] = '$'
         then
             let l = String.split_on_char '.' str in
-            Abs (string_from_to (List.hd l) 2 (String.length (List.hd l)),
+            Abstraction (string_from_to (List.hd l) 2 (String.length (List.hd l)),
                 razbor (string_from_to (list_to_string (List.tl l)) 0 ((String.length (list_to_string (List.tl l)))-1)))
         else
             let l = String.split_on_char ' ' str in
-                Ayy ( razbor (string_from_to (List.hd l) 1 (String.length (List.hd l))),
+                Application ( razbor (string_from_to (List.hd l) 1 (String.length (List.hd l))),
                     razbor (string_from_to (list_to_string (List.tl l)) 0 ((String.length (list_to_string (List.tl l)))-1)))
 
     else
-        Var str ;;
+        Variable str ;;
 
 let rec print_lambda lam = match lam with
-    | Var s -> print_string s
-    | Ayy (l1,l2) -> print_string "("; print_lambda l1; print_string " "; print_lambda l2; print_string ")"
-    | Abs (s,l) -> print_string ("(\\" ^ s ^ "."); print_lambda l; print_string ")" ;;
+    | Variable s -> print_string s
+    | Application (l1,l2) -> print_string "("; print_lambda l1; print_string " "; print_lambda l2; print_string ")"
+    | Abstraction (s,l) -> print_string ("(\\" ^ s ^ "."); print_lambda l; print_string ")" ;;
 
 let r = ref 0 ;;
 
-let rec new_variable l = match l with
-    | Var x -> r := !r+1
-    | Ayy (x,y) -> new_variable x; new_variable y
-    | Abs (x,l) -> r:= !r+1; new_variable l ;;
+let rec new_Variableiable l = match l with
+    | Variable x -> r := !r+1
+    | Application (x,y) -> new_Variableiable x; new_Variableiable y
+    | Abstraction (x,l) -> r:= !r+1; new_Variableiable l ;;
 
-let unic_variable l = r:=0; new_variable l; "."^(string_of_int (!r)) ;;
+let unic_Variableiable l = r:=0; new_Variableiable l; "."^(string_of_int (!r)) ;;
 
 
 let rec alpha_eq x y= match x with
-    | Var a ->
-        if y = Var a
+    | Variable a ->
+        if y = Variable a
         then true
         else false
-    | Ayy (a,b) ->  (match y with
-		| Ayy (c,d) ->
+    | Application (a,b) ->  (match y with
+		| Application (c,d) ->
             if alpha_eq a c && alpha_eq b d
             then true
             else false
 		| _-> false)
-    | Abs (a,l) -> (match y with
-		| Abs (b,k) ->
-            if alpha_eq (rep a (unic_variable l) l) (rep b (unic_variable k) k)
+    | Abstraction (a,l) -> (match y with
+		| Abstraction (b,k) ->
+            if alpha_eq (rep a (unic_Variableiable l) l) (rep b (unic_Variableiable k) k)
             then true
             else false
 		| _-> false) ;;
@@ -73,6 +73,6 @@ let check_alpha_eq s t =
     else print_string "False\n";;
 
 
-(*print_lambda (Ayy (Var "x", Abs ("y", Ayy (Var "z", Abs ("y", Var "x"))))) ;;
+(*print_lambda (Application (Variable "x", Abstraction ("y", Application (Variable "z", Abstraction ("y", Variable "x"))))) ;;
 print_string "\n" ;;*)
-(*check_alpha_eq (Var "x") (Var "x") ;;*)
+(*check_alpha_eq (Variable "x") (Variable "x") ;;*)
