77e73c63d7a055c28ba1f18cd07d0c13f5b39a4c
pashatsyganenko <pashatsyganenko@yandex.ru>
2020-09-30 10:37:34 +0300

    Домашнее задание на 30.09.2020

---
+++ 28092020.ml
@@ -0,0 +1,94 @@
+type morse = Star | ThreeStars | Space | ThreeSpaces | NineSpaces ;;
+
+let rec razbor s = if s = "" then [] else let n = String.length s in match s.[0] with
+    | '*' -> if n > 2 && s.[1] = '*' && s.[2] = '*'
+             then [ThreeStars] @ (razbor (String.sub s 3 (n-3)))
+             else
+                if n = 1
+                then [Star]
+                else [Star] @ (razbor (String.sub s 1 (n-1)))
+    | ' ' -> if n > 8 && List.for_all (fun c -> c=' ') (List.init 8 (fun i -> s.[i+1]))
+             then [NineSpaces] @ (razbor (String.sub s 9 (n-9)))
+             else
+                 if n > 2 && s.[1] = ' ' && s.[2] = ' '
+                 then [ThreeSpaces] @ (razbor (String.sub s 3 (n-3)))
+                 else
+                    if n=1
+                    then [Space]
+                    else [Space] @ (razbor (String.sub s 1 (n-1)))
+    | _ -> failwith "Inalid morse char" ;;
+
+
+let print_morse s =
+List.iter (fun x -> match x with
+    | Star -> print_string "*\n"
+    | ThreeStars -> print_string "***\n"
+    | Space -> print_string " \n"
+    | ThreeSpaces -> print_string "   \n"
+    | NineSpaces -> print_string "         \n"
+    ) (razbor s) ;;
+
+let hash1 = Hashtbl.create 100 ;;
+let hash2 = Hashtbl.create 100 ;;
+let morse_rus = [
+    ([Star; ThreeStars],"А");
+    ([ThreeStars; Star; Star; Star],"Б");
+    ([Star; ThreeStars; ThreeStars],"В");
+    ([ThreeStars; ThreeStars; Star],"Г");
+    ([ThreeStars; Star; Star],"Д");
+    ([Star],"Е");
+    ([Star; Star; Star; ThreeStars],"Ж");
+    ([ThreeStars; ThreeStars; Star; Star],"З");
+    ([Star; Star],"И");
+    ([Star; ThreeStars; ThreeStars; ThreeStars],"Й");
+    ([ThreeStars; Star; ThreeStars],"К");
+    ([Star; ThreeStars; Star; Star],"Л");
+    ([ThreeStars; ThreeStars],"М");
+    ([ThreeStars; Star],"Н");
+    ([ThreeStars; ThreeStars; ThreeStars],"О");
+    ([Star; ThreeStars; ThreeStars; Star],"П");
+    ([Star; ThreeStars; Star],"Р");
+    ([Star; Star; Star],"С");
+    ([ThreeStars],"Т");
+    ([Star; Star; ThreeStars],"У");
+    ([Star; Star; ThreeStars; Star],"Ф");
+    ([Star; Star; Star; Star],"Х");
+    ([ThreeStars; Star; ThreeStars; Star],"Ц");
+    ([ThreeStars; ThreeStars; ThreeStars; Star],"Ч");
+    ([ThreeStars; ThreeStars; ThreeStars; ThreeStars],"Ш");
+    ([ThreeStars; ThreeStars; Star; ThreeStars],"Щ");
+    ([ThreeStars; ThreeStars; Star; ThreeStars; ThreeStars],"Ъ");
+    ([ThreeStars; Star; ThreeStars; ThreeStars],"Ы");
+    ([ThreeStars; Star; Star; ThreeStars],"Ь");
+    ([Star; Star; ThreeStars; Star; Star],"Э");
+    ([Star; Star; ThreeStars; ThreeStars],"Ю");
+    ([Star; ThreeStars; Star; ThreeStars],"Я")] ;;
+
+let eng = ["A"; "B"; "W"; "G"; "D"; "E"; "V"; "Z"; "I"; "J"; "K"; "L";
+"M"; "N"; "O"; "P"; "R"; "S"; "T"; "U"; "F"; "H"; "C"; "_"; "_"; "Q"; "_"; "Y"; "X"; "_"; "_"; "_"] ;;
+
+for i = 0 to 31 do
+    Hashtbl.add hash1 (fst (List.nth morse_rus i)) (snd (List.nth morse_rus i))
+    done ;;
+
+for i = 0 to 31 do
+    Hashtbl.add hash2 (fst (List.nth morse_rus i)) (List.nth eng i)
+    done ;;
+
+let rec lang lm buf hash = match lm with
+    | [] -> Hashtbl.find hash buf
+    | Space :: b -> lang b buf hash
+    | ThreeSpaces :: b -> (Hashtbl.find hash buf)^(lang b [] hash)
+    | NineSpaces :: b -> " "^(lang b [] hash)
+    | a :: b -> lang b (buf @ [a]) hash ;;
+
+let rus m = lang (razbor m) [] hash1 ;;
+let eng m = lang (razbor m) [] hash2 ;;
+(*
+print_string (rus "* *** *** *   * ***   *** *** *** ***   * ***") ; print_newline () ;;
+print_string (eng "* *** *** *   * ***   * * *   * * * *   * ***") ; print_newline () ;;
+*)
+
+let rec int_of_string = function
+    | "" -> 0
+    | s -> let n = String.length s in int_of_char (s.[n-1]) + ((int_of_string (String.sub s 0 (n-1)))*256) ;;
