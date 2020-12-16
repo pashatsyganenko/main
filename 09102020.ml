2d95bc55bc24d72d3724e5f2b184c063021d3111
pashatsyganenko <pashatsyganenko@yandex.ru>
2020-12-16 11:22:13 +0300

    Домашнее задание на 09.11.2020

---
+++ 09102020.ml
@@ -0,0 +1,22 @@
+type tree = Tree of tree list ;;
+
+let check s =
+    let c = ref 0 and f = ref true in
+    for i = 0 to String.length s -1 do
+        if !c<0 then f := false;
+        if s.[i] = '(' then c := !c + 1 else c := !c -1
+    done; !f ;;
+
+let rec parse s n = if n >= String.length s then ([],n) else
+    match s.[n] with
+    | '(' -> let (tl,n) = parse s (n+1) in
+        if s.[n] = ')' then let (tl2,n) = parse s (n+1) in
+        (Tree tl :: tl2,n) else failwith ""
+    | _ -> ([],n) ;;
+
+let rec string_of_tree t = match t with
+    | [] -> ""
+    | Tree a :: b -> "(" ^ (string_of_tree a) ^ ")" ^ (string_of_tree b) ;;
+
+print_string (string_of_tree (fst (parse "()(())()" 0))) ;;
+print_newline () ;;
